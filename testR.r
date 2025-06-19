1b8dbbf0dacf415ba993d470468d33fb620128e6
Tomota113 <itomota11@gmail.com>
2025-06-19 14:43:10 +0100

    Add files via upload

--- testR.r
+++ testR.r
@@ -1,17 +1,18 @@
+# Test R script
 texte <- "test"
-paste("R is",texte)
-test1<- "tomo"
+paste("R is", texte)
+test1 <- "tomo" # nolint
 test2 <- "toto"
 test3 <- "titi"
-#print(paste(test1, test2, test3, sep = "-"))
+# print(paste(test1, test2, test3, sep = "-")) # nolint
 nim1 <- 1
 nim2 <- 2
 nim1 + nim2
-#print(nim1 + nim2) # nolint
-var1 <-var2<-var3 <- 1:10 # nolint
-#print(var1, var2, var3) # nolint # nolint
-#print(paste(var1, var2, var3, sep = "-"))
-x<-45L # nolint
+# print(nim1 + nim2) # nolint
+var1 <- var2 <- var3 <- 1:10 # nolint
+# print(var1, var2, var3) # nolint # nolint
+# print(paste(var1, var2, var3, sep = "-")) # nolint
+x <- 45L # nolint
 is.character(x) # verifie si x est un caractere
 is.logical(x) # verifie si x est un logique
 is.numeric(x) # verifie si x est un nombre
@@ -30,33 +31,83 @@ ceiling(5.3) # retourne le plafond
 floor(5.3) # retourne le plancher
 round(5.3) # retourne l'arrondi
 round(5.7) # retourne l'arrondi
-str<-c("lirili\nlarila\nbombardino\nririli\n","ririli\n","lirili\n","larila\n","bombardino\n")
-#cat(str) # affiche la chaine de caracteres
+str <- c("lirili\nlarila\nbombardino\nririli\n", "ririli\n", "lirili\n", "larila\n", "bombardino\n") # nolint
+# cat(str) # affiche la chaine de caracteres # nolint
 nchar(str) # compte le nombre de caracteres
 grepl("li", str) # verifie si "li" est present dans str
 grep("ri", str) # retourne les indices des occurences de "li"
 gsub("li", "xx", str) # remplace "li" par "xx"
 sub("li", "xx", str) # remplace la premiere occurence de "li" par "xx"
-strsplit(str, "\n") # divise la chaine de caracteres en fonction du separateur "\n"
-ptr<- "i'm atomic \"variable\",big mama \\hh\\"
-#a=15
-#b=20
-#if (a > b) {
-#  print("a est plus grand que b")
-#} else if (a < b) {
-#  print("a est plus petit que b")
-#} else {
+strsplit(str, "\n") # divise la chaine de caracteres en fonction du separateur "\n" # nolint
+ptr <- "i'm atomic \"variable\",big mama \\hh\\" # nolint
+# a=15 # nolint
+# b=20 # nolint
+# if (a > b) {
+#  print("a est plus grand que b") # nolint
+# } else if (a < b) {
+#  print("a est plus petit que b") # nolint
+# } else {
 #  print("a est egal a b") # nolint: commented_code_linter.
-#}
+# }
 a <- 15
 b <- 20
-a%%b # modulo de a par b # nolint
+a %% b # modulo de a par b # nolint
 a %/% b # division entiere de a par b (quotient)
 d <- 5 # affectation locale
 a | b # ou logique (element par element)
 a & b # et logique (element par element)
 a && b # et logique court-circuit
 a || b # ou logique court-circuit
-a 1: 10 # sequence de 1 a 10 # nolint
-a %in% b # verifie si a est dans b
+a <- 1:10 # sequence de 1 a 10 # nolint
+a <- matrix(1:4, nrow = 2) # matrice 2x2
+b <- matrix(5:8, nrow = 2) # matrice 2x2
+a %in% b # verifzie si a est dans b
 a %*% b # produit matriciel de a et b
+a %o% b # produit tensoriel de a et b
+a %x% b # produit tensoriel de a et b
+i <- 1
+while (i < 6) {
+  # print(i) # nolint
+  i <- i + 1
+  if (i == 3) {
+    break # passe a l'iteration suivante
+  }
+}
+i <- 0
+while (i < 6) {
+  i <- i + 1
+  if (i == 3) {
+    next # passe a l'iteration suivante en omettant l'affichage de 3
+  }
+  # print(i) # nolint
+}
+for (x in 1:10) {
+  if (x == 5) {
+    next
+  } else {
+    # print(x) # nolint
+  }
+}
+fruits <- list("apple", "banana", "cherry") # liste de fruits
+for (x in fruits) {
+  # print(x) # affiche chaque fruit # nolint
+}
+fruits <- c("apple", "banana", "cherry") # vecteur de fruits
+for (x in fruits) {
+  # print(x) # affiche chaque fruit # nolint
+}
+dice <- c(1, 2, 3, 4, 5, 6) # vecteur de des
+for (x in dice) {
+  if (x == dice[3]) {
+    next # passe a l'iteration suivante en omettant l'affichage de 3
+  }
+  # print(x) # nolint
+}
+fruits <- list("apple", "banana", "cherry")
+
+for (x in fruits) {
+  if (x == "cherry") {
+    break
+  }
+  # print(x) # nolint
+}
